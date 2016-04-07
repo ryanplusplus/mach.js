ae37c787beeff7acc26d0608846150c5d8503c0f
Waters <h2oboi89@gmail.com>
2016-04-07 09:56:39 -0400

    Any docs

--- src/Any.js
+++ src/Any.js
@@ -1,6 +1,13 @@
 'use strict';
 
+/**
+ * Represents a wild card argument that matches anything.
+ */
 class Any {
+  /**
+   * Returns string representation of this object.
+   * @returns {string} string representation of this object.
+   */
   toString() {
     return '<any>';
   }
