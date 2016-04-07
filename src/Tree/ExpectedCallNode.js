f4f54227c026895b60f4e2f39dc242cdc7b490ff
Waters <h2oboi89@gmail.com>
2016-04-07 09:22:10 -0400

    ExpectedCallNode documentation

--- src/Tree/ExpectedCallNode.js
+++ src/Tree/ExpectedCallNode.js
@@ -2,7 +2,14 @@
 
 var Node = require('./Node.js');
 
+/**
+ * Represents an {@link ExpectedCall} in the {@link Tree}.
+ */
 class ExpectedCallNode extends Node {
+  /**
+   * Creates a new {@link ExpectedCallNode}
+   * @param {ExpectedCall} expectedCall The expected call for this node.
+   */
   constructor(expectedCall) {
     super(expectedCall.name);
     this.expectedCall = expectedCall;
