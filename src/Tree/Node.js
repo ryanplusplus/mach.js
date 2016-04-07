48b905a5fe15fcb166c4c3fe153b48c62bec98f6
Waters <h2oboi89@gmail.com>
2016-04-07 09:23:09 -0400

    Node documentation

--- src/Tree/Node.js
+++ src/Tree/Node.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/**
+ * Abstract base class for all {@link Tree} node types.
+ * @abstract
+ */
 class Node {
   constructor(name) {
     this._name = name;
@@ -7,10 +11,18 @@ class Node {
     this.parent = undefined;
   }
 
+  /**
+   * Gets the human readable name for this {@link Node}.
+   * @returns {string} Human readable name of this node.
+   */
   get name() {
     return this._name;
   }
 
+  /**
+   * Returns this node as a string.
+   * @return {string} This node in string form.
+   */
   toString() {
     var result = '{ ' + this.name;
 
